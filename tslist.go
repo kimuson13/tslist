fc46961c61d356a1d350affc9a1d36ffb5946bfa
kimuson <redram342@gmail.com>
2022-06-10 18:27:30 +0900

    refactor: change name newRes to typeSet

--- tslist.go
+++ tslist.go
@@ -80,40 +80,40 @@ func InterfaceVisitor(name string, interfaceType *ast.InterfaceType, pass *analy
 	visit := Visitor{pass: pass, name: name, result: mp}
 	visit.interfaceVisitor(interfaceType)
 
-	newRes := make(map[string]int)
+	typeSet := make(map[string]int)
 	for _, results := range visit.result {
 		if first := results[0]; len(results) == 1 && first == "any" {
-			newRes[first]++
+			typeSet[first]++
 			continue
 		}
 		for _, result := range results {
 			if result != "any" {
-				newRes[result]++
+				typeSet[result]++
 			}
 		}
 	}
 
-	res := make([]string, 0, len(newRes))
-	if _, ok := newRes["any"]; ok {
-		if len(newRes) == 1 {
+	res := make([]string, 0, len(typeSet))
+	if _, ok := typeSet["any"]; ok {
+		if len(typeSet) == 1 {
 			res = append(res, "any")
 			return VisitorResult{interfaceType.Pos(), name, res}
 		}
 
-		visit.nest -= newRes["any"]
-		newRes["any"]++
+		visit.nest -= typeSet["any"]
+		typeSet["any"]++
 	}
 
-	for typ := range newRes {
+	for typ := range typeSet {
 		if strings.HasPrefix(typ, "~") {
 			val := strings.Trim(typ, "~")
-			if _, ok := newRes[val]; ok {
-				newRes[val]++
+			if _, ok := typeSet[val]; ok {
+				typeSet[val]++
 			}
 		}
 	}
 
-	for typ, num := range newRes {
+	for typ, num := range typeSet {
 		if num == visit.nest {
 			res = append(res, typ)
 		}
